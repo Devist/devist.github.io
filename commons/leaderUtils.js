aca824f6b860f892cb1eb18aff865732a96368d0
Devist <aboutdh89@naver.com>
2020-11-30 00:00:56 +0900

    update

--- commons/leaderUtils.js
+++
@@ -1,9 +0,0 @@
-var LEADER = new Object();
-
-LEADER.getName = function () {
-  return "김롯데";
-}
-
-LEADER.getPosition = function () {
-  return "팀장";
-}
--- commons/staffUtils.js
+++
@@ -1,9 +0,0 @@
-var STAFF = new Object();
-
-STAFF.getName = function () {
-  return "오롯데";
-}
-
-STAFF.getPosition = function () {
-  return "직원";
-}
